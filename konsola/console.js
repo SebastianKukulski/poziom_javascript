b3a539fda46bfa37536c76af682b2241e45ff81e
kajamaja <65953571+SebastianKukulski@users.noreply.github.com>
2023-09-22 12:15:01 +0200

    zz

--- konsola/console.js
+++ konsola/console.js
@@ -2,3 +2,9 @@
 console.log("imie");
 
 console.log("404")
+console.log(window)
+console.group("bulbulator")
+console.log("1szy tekst")
+console.log("2gitekst")
+console.log("3citekst")
+console.groupEnd();;
