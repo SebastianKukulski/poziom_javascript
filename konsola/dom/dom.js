52157354ceac5a909fbd59bf5a2645282d9b7d35
kajamaja <65953571+SebastianKukulski@users.noreply.github.com>
2023-10-02 14:40:21 +0200

    zz

--- konsola/dom/dom.js
+++ konsola/dom/dom.js
@@ -28,9 +28,22 @@ const pElement = document.createElement('p');
 pElement.textContent = 'Kebab na frytkach_-_';
 mainColumn.appendChild(pElement);
 //6
+const footer = document.querySelector('footer');
+const divElement = document.createElement('div');
+divElement.style.backgroundColor = 'pink';
+const pElementInDiv = document.createElement('p');
+pElementInDiv.style.textAlign = 'center';
+pElementInDiv.textContent = '©';
+divElement.appendChild(pElementInDiv);
+footer.appendChild(divElement);
+ //7
+const kolumnaIII = document.querySelector('section.side_column:nth-child(3)');
+const h1ToRemove = kolumnaIII.querySelector('h1');
+h1ToRemove.remove();
 
-
-
-
-
-
+//8
+const newSection = document.createElement('section');
+newSection.style.border = '2px dotted blue';
+newSection.style.padding = '10px 20px';
+newSection.textContent = 'Zderzak';
+document.body.appendChild(newSection);
