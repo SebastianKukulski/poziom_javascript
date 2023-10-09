13a09b77d32ccd81d88d6c2b1c9d96bba706276e
kajamaja <65953571+SebastianKukulski@users.noreply.github.com>
2023-10-09 14:41:26 +0200

    zz

--- konsola/dom/zad4.js
+++ konsola/dom/zad4.js
@@ -7,12 +7,3 @@ function showHide(id) {
         element.style.display = 'block';
     }
 }
-function showHide(id) {
-    var element = document.getElementById(id);
-
-    if (element.style.display == 'block') {
-
-    } else {
-        element.style.display = 'block';
-    }
-}
