ea81413d53651812eb4f177403d08e316297d981
kajamaja <65953571+SebastianKukulski@users.noreply.github.com>
2023-10-09 14:40:04 +0200

    zz

---
+++ konsola/dom/zad4.js
@@ -0,0 +1,18 @@
+function showHide(id) {
+    var element = document.getElementById(id);
+
+    if (element.style.display == 'block') {
+        element.style.display = 'none';
+    } else {
+        element.style.display = 'block';
+    }
+}
+function showHide(id) {
+    var element = document.getElementById(id);
+
+    if (element.style.display == 'block') {
+
+    } else {
+        element.style.display = 'block';
+    }
+}
